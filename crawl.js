ba618fbd88ed3ba42cdcf5ad26400c7620078771
m-peckus <135850434+m-peckus@users.noreply.github.com>
2024-01-15 17:30:37 +0100

    getURLsFromHTMLfunction and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`something went wrong with relative URL ${error.message}`);
+      }
+    } else {
+      // absolute
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`something went wrong with absolute URL ${error.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,5 +37,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 };
